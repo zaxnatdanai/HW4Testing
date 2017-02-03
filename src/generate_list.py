c74f64679ba3459ab8edffd32c22b860eb549ba0
zaxnatdanai <lr.natdanai_st@tni.ac.th>
2017-02-03 10:27:42 +0700

    Add files via upload

---
+++ src/generate_list.py
@@ -0,0 +1,21 @@
+import random
+def generate_list():
+alist = [x for x in range (random.randint(-10,10)) ]
+return alist
+
+"""
+print a generated list
+"""
+def printIt()
+print(generate_list() )
+
+def main():
+printIt()
+"""
+If this script file is called, it will run main() directly
+"""
+if_name_=='_main_':
+print("Test printIt():")
+
+
+main()
